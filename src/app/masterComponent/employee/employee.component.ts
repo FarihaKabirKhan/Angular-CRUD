9307d0e1c45c193b5f75e8693f2a9d8e2737e441
Farihakabirkhan <farihakabirkhan@gmail.com>
2024-08-10 01:30:27 +0600

    ngIf

--- src/app/masterComponent/employee/employee.component.ts
+++ src/app/masterComponent/employee/employee.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal, Signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule,FormsModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
@@ -32,6 +33,33 @@ changeCourse(){
   this.employName='Khan'
   this.MyFatherName.set("Chaiton")
 }
+
+//Structure Directive
+isDivVisible:boolean=true;
+isDiv2Visible:boolean=true;
+
+
+ShowDiv1(){
+this.isDivVisible=true;
+}
+HideDiv1(){
+  this.isDivVisible=false;
+  }
+
+  Toggle(){
+    this.isDiv2Visible=!this.isDiv2Visible;
+    // if(this.isDiv2Visible=true){
+      // this.isDiv2Visible=false;
+
+    // }else{
+      // this.isDiv2Visible=true;
+    // }
+  }
+
+  num1:string='';
+  num2:string='';
+  isActive:boolean=false;
+  SelectedState:string='';
 }
 
 
